549bac5acfce76450e7489cb6331c15585cf598d
presly808 <presly808@gmail.com>
2015-06-27 18:06:37 +0300

    week1 day1

---
+++ src/ua/artcode/ds/dyn/ITree.java
@@ -0,0 +1,13 @@
+package ua.artcode.ds.dyn;
+
+import java.util.Collection;
+
+
+public interface ITree {
+
+    boolean add(Object obj);
+    boolean remove(Object obj);
+    boolean contains(Object obj);
+    void print();
+
+}
