872166bf8750a6b766d1ec952625c32ae434d579
presly808 <presly808@gmail.com>
2015-07-05 13:06:39 +0300

    future merge problem

--- BorisShchipanskyi/src/ua/artcode/week1/Test1.java
+++ BorisShchipanskyi/src/ua/artcode/week1/Test1.java
@@ -4,4 +4,15 @@ package ua.artcode.week1;
  * Created by boris on 7/5/15.
  */
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+        System.out.println("Serhii");
+
+    }
 }
