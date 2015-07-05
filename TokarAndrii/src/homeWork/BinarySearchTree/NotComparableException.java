9f2dd3115bb24f47559d0ea9830aa5730bd17d6d
Tokar Andrii <tokar.andrii@gmail.com>
2015-07-05 22:36:20 +0300

    My binaryTree h/work

---
+++ TokarAndrii/src/homeWork/BinarySearchTree/NotComparableException.java
@@ -0,0 +1,10 @@
+package homeWork.BinarySearchTree;
+
+public class NotComparableException extends RuntimeException {
+
+    public NotComparableException(String message) {
+        super(message);
+    }
+
+
+}
