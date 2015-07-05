7b42347a1ae91f8e194fcb044705f89642cc941f
makaaar <makar7king@ukr.net>
2015-07-05 13:29:21 +0300

    add IOUtils

---
+++ MakarenkoArtem/src/ua/artcode/week1/Tst.java
@@ -0,0 +1,6 @@
+/**
+ * Created by HOME on 05.07.2015.
+ */
+public class Tst {
+
+}
---
+++ MakarenkoArtem/src/ua/artcode/week2/io/IOUtils.java
@@ -0,0 +1,7 @@
+package ua.artcode.week2.io;
+
+/**
+ * Created by HOME on 05.07.2015.
+ */
+public class IOUtils {
+}
