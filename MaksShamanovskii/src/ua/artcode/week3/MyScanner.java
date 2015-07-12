84c8dd9a149adfc812331981e197dbe880d25c7a
shamanovkiy <some4day@gmail.com>
2015-07-12 21:42:20 +0300

    scan

--- MaksShamanovskii/src/ua/artcode/week3/MyScanner.java
+++ MaksShamanovskii/src/ua/artcode/week3/MyScanner.java
@@ -2,76 +2,108 @@ package ua.artcode.week3;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.InputMismatchException;
 
 public class MyScanner {
     private Reader reader;
     private String buf;
+    private char[] buffer = new char[1024];
+    private int start = 0;
+    private int end = 0;
     private int count = 0;
+    private int index = 0;
+    private String delimiter = " ";
 
-    public MyScanner(Reader reader){
+
+    public MyScanner(Reader reader) {
         this.reader = reader;
+        readInBuffer();
     }
 
-    public MyScanner(String line){
+    private void readInBuffer() {
+        if (buf == null) {
+            try {
+                while (reader.ready()) {
+                    buffer[index++] = (char) reader.read();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            char[] temp = buf.toCharArray();
+            for (int i = 0; i < temp.length; i++) {
+                buffer[i] = temp[i];
+            }
+        }
+        findStart();
+    }
+
+    public MyScanner(String line) {
         buf = line;
+        readInBuffer();
     }
 
-    public String nextLine(){
-        if(buf == null){
-            StringBuilder builder = new StringBuilder();
-            try {
-                int k = 0;
-                while((k = reader.read()) != -1) {
-                    builder.append((char)k);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    public String nextLine() {
+        String res = "";
+        for (int i = end; i < buffer.length; i++) {
+            if (buffer[i] != '\u0000') {
+                res += buffer[i];
             }
-            return builder.toString();
         }
-        return buf;
+        return res;
     }
 
-    public String next(){
-        if(buf == null){
-            StringBuilder builder = new StringBuilder();
-            try {
-                int k = 0;
-                while((char) (k = reader.read()) == ' ') {
-                    builder.append((char)k);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return builder.toString();
+    public String next() {
+        findEnd(start);
+        String res = "";
+        for (int i = start; i < end; i++) {
+            res += buffer[i];
         }
-        String[] lines = buf.split(" ");
-        return lines[count++];
+        start = end + 1;
+        findStart();
+        return res;
+    }
+
+    private void findStart() {
+        for (; start < buffer.length; start++) {
+            if (buffer[start] != ' ') {
+                break;
+            }
+        }
+    }
+
+    private void findEnd(int startArr) {
+        for (int i = startArr; i < buffer.length; i++) {
+            if (buffer[i] == ' ' || buffer[i] == '\u0000') {
+                end = i;
+                break;
+            }
+        }
     }
 
     public int nextInt() {
+        return 0;
+    }
+
+
+    public boolean hasNext() {
         if (buf == null) {
-            StringBuilder builder = new StringBuilder();
             try {
-                int k = 0;
-                while ((char) (k = reader.read()) == ' ') {
-                    builder.append((char) k);
-                }
-                return Integer.parseInt(builder.toString());
-            } catch (NumberFormatException e){
-                throw new InputMismatchException(e.getMessage());
-            }
-            catch (IOException e) {
+                return reader.ready();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        String[] lines = buf.split(" ");
-        try {
-            return Integer.parseInt(lines[count++]);
-        }catch (NumberFormatException e){
-            throw new InputMismatchException(e.getMessage());
+        String temp = buf.trim();
+        return !temp.equals("");
+    }
+
+    public boolean hasNextInt() {
+        if (buf == null) {
+
         }
+        String temp = buf.trim();
+
+        return false;
     }
 
 }
