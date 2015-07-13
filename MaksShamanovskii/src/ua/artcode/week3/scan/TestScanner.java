ef5db019e6570a8192a92238df1dd032667d551a
shamanovskiy <some4day@gmail.com>
2015-07-13 22:54:07 +0300

    scan fix

--- MaksShamanovskii/src/ua/artcode/week3/scan/TestScanner.java
+++ MaksShamanovskii/src/ua/artcode/week3/scan/TestScanner.java
@@ -4,14 +4,10 @@ import java.util.Scanner;
 
 public class TestScanner {
     public static void main(String[] args) {
-        MyScanner s = new MyScanner(" 123 we1  dwd                  qw");
-        System.out.println(s.hasNextInt());
+        MyScanner s = new MyScanner("1111  2222       3333");
         System.out.println(s.nextInt());
-        System.out.println(s.hasNextInt());
-        System.out.println(s.next());
-        System.out.println(s.hasNextInt());
-        System.out.println(s.next());
-        System.out.println(s.nextLine());
+        System.out.println(s.nextInt());
+        System.out.println(s.nextInt());
         System.out.println();
 
 
