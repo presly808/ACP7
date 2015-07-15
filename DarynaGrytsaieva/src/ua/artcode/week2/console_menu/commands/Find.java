4cc218e4c184081ed19deae179c70157998bfe93
Daryna_G <grytsaieva@ukr.net>
2015-07-15 11:15:35 +0300

    Command console

---
+++ DarynaGrytsaieva/src/ua/artcode/week2/console_menu/commands/Find.java
@@ -0,0 +1,67 @@
+package ua.artcode.week2.console_menu.commands;
+
+import art_code.console_menu.InvalidCommandException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ * Created by Daryna on 14-Jul-15.
+ */
+public class Find implements Command {
+    private final String COMMAND_NAME = "find";
+
+
+    @Override
+    public String getCommandName() {
+        return COMMAND_NAME;
+    }
+
+    @Override
+    public String run(String currentDir, String commandArgument) throws FileNotFoundException {
+        String searchDir = getSearchDir(currentDir, commandArgument);
+        File rootFile = new File(searchDir);
+
+        System.out.println("---" + find(rootFile, commandArgument));
+
+        return currentDir;
+
+    }
+
+
+    private String find(File file, String fileName) {
+        ArrayList<File> files = new ArrayList<File>();
+        File[] temp = file.listFiles();
+        for (File ff : temp) {
+            files.add(ff);
+        }
+        String path = null;
+        while (!files.isEmpty()) {
+            if (files.get(0).getName().equals(fileName)) {
+                path = files.get(0).getPath();
+            }
+            if (files.get(0).isDirectory()) {
+
+                for (File ff : files.get(0).listFiles())
+                    files.add(ff);
+            }
+            files.remove(0);
+        }
+        return path == null ? "File/dir not found." : path;
+
+
+    }
+
+    private String getSearchDir(String currentDir, String commandArgument) {
+
+        if (commandArgument.contains(" ") || commandArgument.isEmpty()) {
+            throw new InvalidCommandException("Invalid argument.");
+
+        } else {
+            //return CommandConsole.getRootDir();
+            return currentDir;
+        }
+    }
+
+}
