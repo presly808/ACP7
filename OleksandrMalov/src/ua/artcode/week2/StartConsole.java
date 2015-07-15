185375fdbf74dab4593a1ccfa51684b4596045e2
Oleksandr Malov <malov.oleksandr@gmail.com>
2015-07-15 14:07:44 +0300

    Homework w2 cd logic

---
+++ OleksandrMalov/src/ua/artcode/week2/StartConsole.java
@@ -0,0 +1,12 @@
+package ua.artcode.week2;
+
+/**
+ * Created by Олександр on 08.07.2015.
+ */
+public class StartConsole {
+
+    public static void main(String[] args) {
+
+        Console console = new Console();
+    }
+}
