2baf1a69485c518c4dc83365bac8d34802455059
Pikolo88 <masca231@gmail.com>
2015-07-18 12:00:32 +0300

    scannerbegining

--- MykolaRadaguz/src/ua/artcode/week3/homework/MyScanner.java
+++ MykolaRadaguz/src/ua/artcode/week3/homework/MyScanner.java
@@ -1,22 +1,55 @@
 package ua.artcode.week3.homework;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
+import java.io.*;
 import java.util.Scanner;
 
 /**
  * Created by pikolo on 15.07.15.
  */
-public class MyScanner {
+public class MyScanner implements IScanner {
 
-    private Reader reader;
-    private String nextString;
-    private int nextInt;
-    private InputStream inputStream;
+    private Reader input;
+    private String delimiter;
+    private char[] buff = new char[1024];
+    private int startPos = 0;
+    private int endPos = 0;
+
+    public MyScanner(Reader reader) {
+        this.input = reader;
+    }
+
+    public MyScanner(String source) {
+        input = new StringReader(source);
+    }
+
+    public void fillInBuff() {
+        try {
+            endPos = input.read(buff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public String next() {
-        return null;
+        if (startPos > endPos) {
+            fillInBuff();
+        }
+
+        String next = "";
+        char read = 0;
+        int iter = startPos;
+        try {
+            while ((read = (char) input.read()) != -1 || read != ' ') {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return next.isEmpty() ? null : next;
+    }
+
+    public void remove() {
+
     }
 
     public int nextInt() {
@@ -35,14 +68,12 @@ public class MyScanner {
         return true;
     }
 
-    public void useDelimeter(String delimiter) {
+    public void useDelimiter(String delimiter) {
 
     }
 
     public void close() {
 
     }
-
-
 
 }
