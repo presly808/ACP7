65cc852d32e3c9c522a031afec7f8972902bf5b4
Pikolo88 <masca231@gmail.com>
2015-07-18 11:06:26 +0300

    new commit

---
+++ MykolaRadaguz/src/ua/artcode/week3/homework/MyScanner.java
@@ -0,0 +1,48 @@
+package ua.artcode.week3.homework;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.Scanner;
+
+/**
+ * Created by pikolo on 15.07.15.
+ */
+public class MyScanner {
+
+    private Reader reader;
+    private String nextString;
+    private int nextInt;
+    private InputStream inputStream;
+
+    public String next() {
+        return null;
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+    public String nextLine() {
+        return null;
+    }
+
+    public boolean hasNext() {
+        return true;
+    }
+
+    public boolean hasNextInt() {
+        return true;
+    }
+
+    public void useDelimeter(String delimiter) {
+
+    }
+
+    public void close() {
+
+    }
+
+
+
+}
