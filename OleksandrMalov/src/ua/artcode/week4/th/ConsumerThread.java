e91e62fbeac81e382bdabd0a563578c779f551f0
Oleksandr Malov <malov.oleksandr@gmail.com>
2015-07-19 12:07:52 +0300

    week42

---
+++ OleksandrMalov/src/ua/artcode/week4/th/ConsumerThread.java
@@ -0,0 +1,18 @@
+package ua.artcode.week4.th;
+
+/**
+ * Created by Олександр on 19.07.2015.
+ */
+public class ConsumerThread  implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5 ; i++) {
+            
+        }
+
+    }
+    public void money(){
+
+    }
+}
