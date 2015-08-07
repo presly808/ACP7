ed2879e45fc76df334763e9429399ded20e9db64
Tokar Andrii <tokar.andrii@gmail.com>
2015-08-07 17:21:15 +0300

    h/w chat

---
+++ TokarAndrii/src/week4/homeWorkWeek4/myChat/controller/IOSerialMessage.java
@@ -0,0 +1,82 @@
+package week4.homeWorkWeek4.myChat.controller;
+
+import week4.homeWorkWeek4.myChat.model.MessageMy;
+
+import java.io.*;
+
+/**
+ * Created by Пользователь on 07.08.15.
+ */
+public class IOSerialMessage implements IOHelper {
+
+    @Override
+    public void save(String path, MessageMy messageMy) {
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            oos.writeObject(messageMy);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public MessageMy load(String path) {
+
+        MessageMy ret = null;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            ret = (MessageMy) ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return ret;
+    }
+}
