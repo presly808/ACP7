0a8ac28d123ac36ad963d6b09ab025b0285ad81e
presly808 <presly808@gmail.com>
2015-08-17 21:05:24 +0300

    change to maven structure

---
+++ SerhiiBilobrov/src/main/java/ua/artcode/Test.java
@@ -0,0 +1,15 @@
+package ua.artcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by serhii on 05.07.15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> sss = new ArrayList<>();
+
+    }
+}
