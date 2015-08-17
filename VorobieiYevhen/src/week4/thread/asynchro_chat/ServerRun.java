6c62a80ec725598dea8c8057a0e3c24ec4c42f3e
gorobec <yevhenijvorobiei@gmail.com>
2015-08-17 21:12:23 +0300

    delete projects

--- VorobieiYevhen/src/week4/thread/asynchro_chat/ServerRun.java
+++
@@ -1,9 +0,0 @@
-package week4.thread.asynchro_chat;
-
-import java.io.IOException;
-
-public class ServerRun {
-    public static void main(String[] args) throws IOException {
-        new Server().start();
-    }
-}
