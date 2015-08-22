825e19112b676e58ae84b2777276da849fb740b6
presly808 <presly808@gmail.com>
2015-08-22 14:19:13 +0300

    week7 day1 part1

---
+++ src/main/java/ua/artcode/week7/jpa/InitEntityManagerFactory.java
@@ -0,0 +1,14 @@
+package ua.artcode.week7.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by serhii on 22.08.15.
+ */
+public class InitEntityManagerFactory {
+
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-unit");
+    }
+}
