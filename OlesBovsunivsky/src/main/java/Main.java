189df0643dee83bb4b60a8f83f6a981f01cfde20
OlesBov <oles123@bk.ru>
2015-08-23 11:27:26 +0300

    OlesM

--- OlesBovsunivsky/src/main/java/Main.java
+++ OlesBovsunivsky/src/main/java/Main.java
@@ -1,9 +1,7 @@
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- * Created by Legion on 22.08.2015.
- */
+
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-unit");
