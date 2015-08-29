fdd9ca5a2a56560f24637e7ef940d1be1ea36525
presly808 <presly808@gmail.com>
2015-08-29 12:30:54 +0300

    week8 day1

--- SerhiiBilobrov/src/main/java/ua/artcode/week8/ioc/ForInject.java
+++ SerhiiBilobrov/src/main/java/ua/artcode/week8/ioc/ForInject.java
@@ -1,7 +1,11 @@
 package ua.artcode.week8.ioc;
 
-/**
- * Created by serhii on 29.08.15.
- */
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface ForInject {
 }
