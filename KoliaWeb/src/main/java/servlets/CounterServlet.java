f3ea15e89b8a0cf0ca3ebe0aadbe1a348ead410a
Pikolo88 <masca231@gmail.com>
2015-09-05 13:20:32 +0300

    Servlets

--- KoliaWeb/src/main/java/servlets/CounterServlet.java
+++ KoliaWeb/src/main/java/servlets/CounterServlet.java
@@ -6,18 +6,18 @@ import java.io.PrintWriter;
 /**
  * Created by pikolo on 05.09.15.
  */
-public class CounterServlet extends HttpServlet{
-
-    @Override
-    public void init() throws ServletException {
-        System.out.println("SERVLET CREATED");
-    }
-
-
-
-    @Override
-    public void destroy() {
-        System.out.println("SERVLET DESTROYED");
-    }
-
-}
+//public class CounterServlet extends HttpServlet{
+//
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("SERVLET CREATED");
+//    }
+//
+//
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("SERVLET DESTROYED");
+//    }
+//
+//}
--- KoliaWeb/src/main/java/servlets/HelloWorldServlet.java
+++ KoliaWeb/src/main/java/servlets/HelloWorldServlet.java
@@ -6,31 +6,31 @@ import java.io.PrintWriter;
 /**
  * Created by pikolo on 05.09.15.
  */
-public class HelloWorldServlet extends  HttpServlet{
-
-    @Override
-    public void init() throws ServletException {
-        System.out.println("SERVLET CREATED");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String info = String.format("method %s, remote %s, query %s", req.getMethod(),
-                req.getRemoteAddr(),
-                req.getQueryString());
-
-        PrintWriter pw = resp.getWriter();
-        pw.println("<div><h1>User Info</h1>" +
-                "Your info from server " + info +
-                "<div>");
-
-        pw.flush();
-
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("SERVLET DESTROYED");
-    }
-
-}
+//public class HelloWorldServlet extends  HttpServlet{
+//
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("SERVLET CREATED");
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String info = String.format("method %s, remote %s, query %s", req.getMethod(),
+//                req.getRemoteAddr(),
+//                req.getQueryString());
+//
+//        PrintWriter pw = resp.getWriter();
+//        pw.println("<div><h1>User Info</h1>" +
+//                "Your info from server " + info +
+//                "<div>");
+//
+//        pw.flush();
+//
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("SERVLET DESTROYED");
+//    }
+//
+//}
