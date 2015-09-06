00a82c28ed5ef7e8ca3bcd849dbdbc55e9c418d8
makaaar <makar7king@ukr.net>
2015-09-06 10:03:39 +0300

    week 9

---
+++ MakarenkoArtem/src/main/java/ua/artcode/ClassWork/Week_9/http/TestHttpRequest.java
@@ -0,0 +1,7 @@
+package ua.artcode.ClassWork.Week_9.http;
+
+/**
+ * Created by HOME on 05.09.2015.
+ */
+public class TestHttpRequest {
+}
---
+++ MakarenkoArtemWeb/src/main/java/servlet/HelloWordServlet.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by HOME on 05.09.2015.
+ */
+public class HelloWordServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("SERVLET CREATED");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String info = String.format("method %s, remote %s, query %s", req.getMethod(),
+                req.getRemoteAddr(),
+                req.getQueryString());
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("<div><h1>User INfo</h1>" +
+                "Your info from server " + info +
+                "<div>");
+
+        pw.flush();
+
+    }
+}
