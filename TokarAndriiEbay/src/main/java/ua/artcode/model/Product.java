db0da4b6ee8e24375603b0989c7f917da16831c3
tokarandrii <tokar.andrii@gmail.com>
2015-09-07 21:47:55 +0300

    week10 doing myproject

---
+++ TokarAndriiEbay/src/main/java/ua/artcode/model/Product.java
@@ -0,0 +1,72 @@
+package ua.artcode.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product extends IdEntity{
+
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private String title;
+    @Column()
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User owner;
+
+
+    public Product() {
+    }
+
+    public Product(double price, String title, String desc) {
+        this.price = price;
+        this.title = title;
+        this.description = desc;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("id=").append(getId());
+        sb.append(", price=").append(price);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
